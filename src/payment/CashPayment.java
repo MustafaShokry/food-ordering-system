b6ab3b026c4d711404db2e25bc598d4e535b3f3d
MustafaShokry <mostafashokry2222@gmail.com>
2026-07-09 01:41:28 +0300

    Implement cash payment

---
+++ src/payment/CashPayment.java
@@ -0,0 +1,8 @@
+public class CashPayment implements PaymentMethod {
+
+    @Override
+    public boolean pay(double amount) {
+        System.out.println("Paid $" + amount + " using Cash.");
+        return true;
+    }
+}
