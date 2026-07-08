fea9d54d90d86907d5295bd85d3fe783c4c4048a
MustafaShokry <mostafashokry2222@gmail.com>
2026-07-09 00:53:40 +0300

    Add OrderItem domain model

---
+++ src/model/OrderItem.java
@@ -0,0 +1,23 @@
+import src.model.MenuItem;
+
+public class OrderItem {
+    private MenuItem menuItem;
+    private int quantity;
+
+    public OrderItem(MenuItem menuItem, int quantity) {
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getSubTotalPrice() {
+        return menuItem.getPrice() * quantity;
+    }
+}
