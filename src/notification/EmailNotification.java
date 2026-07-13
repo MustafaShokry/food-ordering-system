38285a80edb001ce0e6dc38e5b335ab04427c08a
Mostafa Shokry El-Kamel <mostafashokry2222@gmail.com>
2026-07-13 11:22:32 +0300

    Implement email notification

---
+++ src/notification/EmailNotification.java
@@ -0,0 +1,6 @@
+public class EmailNotification implements NotificationChannel {
+    @Override
+    public void sendNotification(Customer customer, String message) {
+        System.out.println("Sent a notification through email to" + Customer.getEmail());
+    }
+}
