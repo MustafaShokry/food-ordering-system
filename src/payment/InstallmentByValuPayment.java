5c7d27da143e2f8990562d5a927b881007796bf3
Mostafa Shokry El-Kamel <mostafashokry2222@gmail.com>
2026-07-13 11:37:00 +0300

    Add installment by valu payment method

---
+++ src/payment/InstallmentByValuPayment.java
@@ -0,0 +1,8 @@
+public class InstallmentByValuPayment implements PaymentMethod {
+
+    @Override
+    public boolean pay(double amount) {
+        System.out.println("Paid $" + amount + " in installment by valu.");
+        return true;
+    }
+}
